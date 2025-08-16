d483f89c518da31ba1e09793c6989ec409177da9
Suman KC <sumankc844@gmail.com>
2025-08-16 17:19:43 +0545

    ecommerce application

--- lib/pages/accountpage.dart
+++ lib/pages/accountpage.dart
@@ -4,7 +4,7 @@ class MyAccount extends StatelessWidget {
   const MyAccount({super.key});
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -13,73 +13,86 @@ class MyAccount extends StatelessWidget {
           },
           icon: Icon(Icons.arrow_back_sharp),
         ),
-        title: Title(
-          color: Colors.black,
-          child: const Text("My Account"),
-        ),
+        title: Title(color: Colors.black, child: const Text("My Account")),
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 10,horizontal: 18),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 18),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Basic Information",style: TextStyle(color: Colors.black,fontSize: 20),),
-            SizedBox(height: 20,),
+            Text(
+              "Basic Information",
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+            SizedBox(height: 20),
             Material(
               elevation: 10,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
               child: ListTile(
-                leading: Icon(Icons.person,),
-                title: Text("My Name",style: TextStyle(fontSize: 19),),
-                subtitle: Text("My Numeber",style: TextStyle(fontSize: 19)),
-                
-                
-                
+                leading: Icon(Icons.person),
+                title: Text("My Name", style: TextStyle(fontSize: 19)),
+                subtitle: Text("My Numeber", style: TextStyle(fontSize: 19)),
               ),
             ),
-            SizedBox(height: 40,),
-            Text("Security Setting",style: TextStyle(color: Colors.black,fontSize: 20),),
-            SizedBox(height: 20,),
+            SizedBox(height: 40),
+            Text(
+              "Security Setting",
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+            SizedBox(height: 20),
             Material(
               elevation: 10,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
               child: ListTile(
-                leading: Icon(Icons.logout,),
-                title: Text("Logout",style: TextStyle(fontSize: 19),),
-                subtitle: Text("Logout",style: TextStyle(fontSize: 16),),
-                trailing: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_forward)),
-                
-                
+                leading: Icon(Icons.logout),
+                title: Text("Logout", style: TextStyle(fontSize: 19)),
+                subtitle: Text("Logout", style: TextStyle(fontSize: 16)),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_forward),
+                ),
               ),
             ),
-            SizedBox(height: 30,),
+            SizedBox(height: 30),
             Material(
               elevation: 10,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
               child: ListTile(
-                leading: Icon(Icons.person,),
-                title: Text("Change Passeord",style: TextStyle(fontSize: 19),),
-                subtitle: Text("Change Password",style: TextStyle(fontSize: 16),),
-                trailing: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_forward)),
-                
-                
+                leading: Icon(Icons.person),
+                title: Text("Change Passeord", style: TextStyle(fontSize: 19)),
+                subtitle: Text(
+                  "Change Password",
+                  style: TextStyle(fontSize: 16),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_forward),
+                ),
               ),
             ),
-            SizedBox(height: 30,),
-           Material(
+            SizedBox(height: 30),
+            Material(
               elevation: 10,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
               child: ListTile(
-                leading: Icon(Icons.person,),
-                title: Text("Security Tips",style: TextStyle(fontSize: 19),),
-                subtitle: Text("Security Tips",style: TextStyle(fontSize: 16),),
-                trailing: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_forward)),
-                
-                
+                leading: Icon(Icons.person),
+                title: Text("Security Tips", style: TextStyle(fontSize: 19)),
+                subtitle: Text("Security Tips", style: TextStyle(fontSize: 16)),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_forward),
+                ),
               ),
             ),
-        
           ],
         ),
       ),
